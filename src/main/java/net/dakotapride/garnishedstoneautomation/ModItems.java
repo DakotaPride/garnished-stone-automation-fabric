ef59f685986c9e25a1a42c8e9c1117d474381ff8
Dakota <rjtube13@gmail.com>
2024-07-16 16:58:32 -0500

    Initial Commit

--- src/main/java/net/dakotapride/garnishedstoneautomation/ModItems.java
+++ src/main/java/net/dakotapride/garnishedstoneautomation/ModItems.java
@@ -2,16 +2,17 @@ package net.dakotapride.garnishedstoneautomation;
 
 import static net.dakotapride.garnishedstoneautomation.GarnishedStoneAutomation.REGISTRATE;
 
+import com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem;
 import com.tterrag.registrate.util.entry.ItemEntry;
 
 import net.minecraft.world.item.Item;
 
 public class ModItems {
 	// Incomplete Processing Items
-	public static final ItemEntry<Item> INCOMPLETE_CRIMSITE = REGISTRATE.get().item("incomplete_crimsite", Item::new).register();
-	public static final ItemEntry<Item> INCOMPLETE_VERIDIUM = REGISTRATE.get().item("incomplete_veridium", Item::new).register();
-	public static final ItemEntry<Item> INCOMPLETE_ASURINE = REGISTRATE.get().item("incomplete_asurine", Item::new).register();
-	public static final ItemEntry<Item> INCOMPLETE_OCHRUM = REGISTRATE.get().item("incomplete_ochrum", Item::new).register();
+	public static final ItemEntry<SequencedAssemblyItem> INCOMPLETE_CRIMSITE = REGISTRATE.get().item("incomplete_crimsite", SequencedAssemblyItem::new).register();
+	public static final ItemEntry<SequencedAssemblyItem> INCOMPLETE_VERIDIUM = REGISTRATE.get().item("incomplete_veridium", SequencedAssemblyItem::new).register();
+	public static final ItemEntry<SequencedAssemblyItem> INCOMPLETE_ASURINE = REGISTRATE.get().item("incomplete_asurine", SequencedAssemblyItem::new).register();
+	public static final ItemEntry<SequencedAssemblyItem> INCOMPLETE_OCHRUM = REGISTRATE.get().item("incomplete_ochrum", SequencedAssemblyItem::new).register();
 
 	// Vehement Clusters
 	public static final ItemEntry<Item> CRIMSITE_CLUSTER = REGISTRATE.get().item("crimsite_cluster", Item::new).register();
